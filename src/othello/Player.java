b1efea59507bb62c09b9a0253b232340d4252789
mijacke <lassumario2002@gmail.com>
2022-12-03 23:03:43 +0100

    added java doc for the Player class

--- src/othello/Player.java
+++ src/othello/Player.java
@@ -1,19 +1,43 @@
 package othello;
+
 import java.awt.*;
 
+/**
+ * Class for creating players who will play against each other.
+ * This class has 2 private immutable fields color and name, which
+ * are specific for each player.
+ *
+ * @author Mário Laššú
+ */
 public class Player {
     private final Color color;
     private final String name;
 
-    public Player(Color c, String name){
+    /**
+     * Constructor for creating a player.
+     *
+     * @param c    is the color of the player
+     * @param name is the name of the player
+     */
+    public Player(Color c, String name) {
         this.color = c;
         this.name = name;
     }
 
+    /**
+     * Getter of the color.
+     *
+     * @return the player's color
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * Getter of the name.
+     *
+     * @return the player's name
+     */
     public String getName() {
         return name;
     }
