5beea42e3955146d769c47c36f189dc948b070fc
mijacke <lassumario2002@gmail.com>
2022-12-03 22:54:45 +0100

    added java doc for the Main class

--- src/othello/OthelloStarter.java
+++ src/othello/OthelloStarter.java
@@ -1,5 +1,12 @@
 package othello;
 
+/**
+ * Through the Main class we can run the game even outside BlueJ.
+ * It contains one static main method (core method of the program
+ * which calls all others, and it also can't return values). This
+ * method can be invoked by Java Virtual Machine without instantiating
+ * the class.
+ */
 public class OthelloStarter {
     public static void main(String[] args) {
         new Othello();
